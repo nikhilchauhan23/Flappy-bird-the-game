a49ca46401d32f56956588e1aa53f8342365cb9e
Nikhil Verma <91723032+nikhilchauhan23@users.noreply.github.com>
2022-04-07 16:19:31 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+   move pipes to the left
+        for upperPipe , lowerPipe in zip(upperPipes, lowerPipes):
+            upperPipe['x'] += pipeVelX
+            lowerPipe['x'] += pipeVelX
+
+        # Add a new pipe when the first is about to cross the leftmost part of the screen
+        if 0<upperPipes[0]['x']<5:
+            newpipe = getRandomPipe()
+            upperPipes.append(newpipe[0])
+            lowerPipes.append(newpipe[1])
+
+        # if the pipe is out of the screen, remove it
+        if upperPipes[0]['x'] < -GAME_SPRITES['pipe'][0].get_width():
+            upperPipes.pop(0)
+            lowerPipes.pop(0)
+        
+        Lets blit our sprites now
